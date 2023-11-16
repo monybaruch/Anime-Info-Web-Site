279982d93bfbc47de381dc111871f2768d28964b
מוני ברוך <monybaruch@gmail.com>
2023-11-16 02:04:14 +0200

    adding the searchAnime state and setting it up to change when a user is searching an anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [searchAnime, setSearchAnime] = useState('nope');
   const [animeData, setAnimeData] = useState();
   const baseUrl = 'https://api.jikan.moe/v4';
   const getAnimeData = async () => {
     const res = await fetch(`${baseUrl}/top/anime?filter=bypopularity`);
     const resData = await res.json();
     console.log(resData.data);
+
     setAnimeData(resData.data);
   };
   useEffect(() => {
@@ -17,7 +19,13 @@ function App() {
       <div className="header">
         <h1>My Anime Info</h1>
         <div className="anime-search">
-          <input type="search" placeholder="Search your favorite anime " />
+          <input
+            type="search"
+            placeholder="Search your favorite anime!"
+            onChange={(e) => {
+              setSearchAnime(e.target.value);
+            }}
+          />
         </div>
       </div>
     </>
